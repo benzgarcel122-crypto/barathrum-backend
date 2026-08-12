376ba8cd9a7d996df78eca0c2bf1e861caec78c8
benzgarcel122-crypto <benzgarcel122@gmail.com>
2026-08-12 09:03:01 +0800

    Update urls.py

--- machines/urls.py
+++ machines/urls.py
@@ -7,4 +7,5 @@ app_name = "machines"
 urlpatterns = [
     path("api/box/validate-license/", views.validate_license_view, name="validate_license"),
     path("api/box/license-points/", views.license_points_view, name="license_points"),
+    path("api/box/unbind-license/", views.unbind_license_view, name="unbind_license"),
 ]
